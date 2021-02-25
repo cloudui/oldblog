20951be83487a6fe141cba0aec14756097dd9f77
Eric <eric@echen.io>
2021-02-25 10:31:58 -0500

    search edits and putting post preview in new html file

--- posts/urls.py
+++ posts/urls.py
@@ -8,7 +8,7 @@ urlpatterns = [
     path('search', SearchResultsListView.as_view(), name='search_results'),
     path('classes/', ZoomLinks.as_view(), name='zoom'),
     path('images/', ImageView.as_view(), name='images'),
-    path('preview/<slug:slug>/', PostPreviewView.as_view(), name='post_preview'),
+    path('preview/<slug:slug>/', PostPreviewView.as_view(), name='preview'),
     path('<slug:slug>/', PostDetailView.as_view(), name='detail'),
-    path('<slug:slug>/edit', PostUpdateView.as_view(), name='update'),
+    path('<slug:slug>/edit/', PostUpdateView.as_view(), name='update'),
 ]
